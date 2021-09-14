20ab887da74ecc0c0a919358f6b308102677d645
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-09-14 11:41:36 +0100

    added navbar

---
+++ src/Components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="navbar p-4 bg-gray-600 text-white font-serif">
+      <div className="py-3">
+        <ul className="flex justify-center items-center">
+          <li className="px-3">Home</li>
+          <li className="px-3">About</li>
+          <li className="px-3">Anime</li>
+        </ul>
+      </div>
+
+
+    </div>
+    
+  )
+}
+
+export default Navbar
