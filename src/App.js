dd84e9f39a4e282d0198de7d522807cfd8b55cc3
Mohamed-N-Mohamed <Mohamed-N-Mohamed@outlook.com>
2021-09-16 15:32:44 +0100

    added show section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Navbar from "./Components/Navbar";
 import Search from "./Components/Search"
+import Show from "./Components/Show";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
       <Search/>
-    
-
+      <Show/>
     </div>
   );
 }
